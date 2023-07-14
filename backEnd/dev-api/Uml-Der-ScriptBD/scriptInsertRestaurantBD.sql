2930ff6484dd19fa67d3c237bc32538f1fb7a26a
Rafael-Ramirez1991 <rafa_zxa10@hotmail.com>
2023-07-14 10:08:42 -0500

    proyecto integrador completo

---
+++ backEnd/dev-api/Uml-Der-ScriptBD/scriptInsertRestaurantBD.sql
@@ -0,0 +1,86 @@
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.categories (description, name)
+values ("Platos que refleja la pasión y la cultura gastronómica argentina","Comida Argentina");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.categories (description, name)
+values ("Platos que refleja la combinación de sabores y texturas de la cultura gastronómica Mexicana","Comida Mexicana");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.categories (description, name)
+values ("Platos que refleja la riqueza de sabores y generosas porciones de la cultura gastronómica Colombiana","Comida Colombiana");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.categories (description, name)
+values ("Platos que refleja la diversidad, elegancia y autenticidad de la cultura gastronómica Oriental","Comida Oriental");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.categories (description, name)
+values ("Platos frescos y nutritivos que promueve una alimentación equilibrada y contribuye al bienestar general","Comida Saludable");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.categories (description, name)
+values ("Variedad de platos de distintas partes de europa, ricas en sabores","Comida Internacional");
+
+
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.cities (country, department, name)
+values ("Colombia", "Cundinamarca", "Bogota");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.cities (country, department, name)
+values ("Colombia", "Antioquia", "MedelliN");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.cities (country, department, name)
+values ("Colombia", "Valle del Cauca", "Cali");
+
+
+
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.restaurants (address,description,name,url_image,category_id,city_id)
+values ("Calle 69 10a 35 Barrio Quinta Camacho", "Churrasquería, Parrilla, Argentina, Bar de vinos, Sudamericana. Almuerzo, Cena, Bebidas. 11:00 a. m. - 8:00 p. m","La Churrasqueria Beef & Wine","https://media-cdn.tripadvisor.com/media/photo-s/12/30/a2/2e/la-nueva-sede.jpg","1","1");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.restaurants (address,description,name,url_image,category_id,city_id)
+values ("Cl. 15 Nte. #9N-62", "Mexicana, Latina. Almuerzo, Cena, Abierto hasta tarde, Bebidas","Cantina La 15 Granada","https://media-cdn.tripadvisor.com/media/photo-s/26/f4/bd/30/restaurante-para-eventos.jpg","3","2");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.restaurants (address,description,name,url_image,category_id,city_id)
+values ("Autopista Norte #114-44 (2do piso)", "Mediterránea, Internacional, Fusión, Bar, Pub. Almuerzo, Cena, Abierto hasta tarde, Bebidas","Descortés","https://media-cdn.tripadvisor.com/media/photo-f/1c/6f/80/5a/llevamos-la-gastronomia.jpg","6","1");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.restaurants (address,description,name,url_image,category_id,city_id)
+values ("Calle 84bis 13 54 Hotel Morrison 84", "Mexicana, Fusion. Opciones veganas","Guadalupe Latina","https://media-cdn.tripadvisor.com/media/photo-f/1c/58/f1/6a/arepa-de-maiz-peto-blanco.jpg","2","1");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.restaurants (address,description,name,url_image,category_id,city_id)
+values ("Calle 69 10a 35 Barrio Quinta Camacho", "Mexicana, Bar, Contemporánea, Pub, Fusión, Comedores. Almuerzo, Cena, Abierto hasta tarde, Bebidas","La Santa Guadalupe","https://media-cdn.tripadvisor.com/media/photo-s/22/0b/79/80/burro-ahogado-burro-de.jpg","2","2");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.restaurants (address,description,name,url_image,category_id,city_id)
+values ("Carrera 37 # 10 - 8 Esq con Calle 10", "Apto para vegetarianos, Opciones veganas, Opciones sin gluten","Soul Korean Street Food","https://media-cdn.tripadvisor.com/media/photo-s/23/5f/3b/4c/soul-korean-food-medellin.jpg","4","2");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.restaurants (address,description,name,url_image,category_id,city_id)
+values ("Calle 118 5 41 Usaquén", "Desayuno, Almuerzo, Cena, Brunch, Bebidas","Abasto","https://media-cdn.tripadvisor.com/media/photo-p/15/85/17/2d/photo4jpg.jpg","5","1");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.restaurants (address,description,name,url_image,category_id,city_id)
+values ("Carrera 32d 7a # 7a-77 Local 01", "Vegetarianos, Opciones veganas, Opciones sin gluten","Betty's Bowls","https://media-cdn.tripadvisor.com/media/photo-s/17/e3/ee/04/betty-s-bowls-r.jpg","5","2");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.restaurants (address,description,name,url_image,category_id,city_id)
+values ("Carrera 43 E # 11 a - 20 carrera 43 E # 11 A - 20", "Parrilla, Bar de vinos, Argentina, Churrasquería. Almuerzo, Cena, Bebidas.","Restaurante Malevo","https://media-cdn.tripadvisor.com/media/photo-s/07/a9/67/ad/restaurante-malevo.jpg","2","1");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.restaurants (address,description,name,url_image,category_id,city_id)
+values ("Av 8 norte 10-18", "Japonesa, Sushi, Asiática. Apto para vegetarianos. Almuerzo, Cena.","Tanoshii Lounge & Sushi Bar","https://media-cdn.tripadvisor.com/media/photo-s/16/74/54/e3/tanoshii.jpg","4","3");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.restaurants (address,description,name,url_image,category_id,city_id)
+values ("Calle 26n 5an 25", "Café, Contemporánea, Saludable, Fusión. Apto para vegetarianos, Opciones veganas, Opciones sin gluten.","Mascabado","https://media-cdn.tripadvisor.com/media/photo-s/21/9b/b6/b7/opciones-para-toda-la.jpg","5","3");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.restaurants (address,description,name,url_image,category_id,city_id)
+values ("Carrera 49, 52-98 2° Piso", "Colombiana. Bebidas, Almuerzo, Cena","Hacienda - Junin","https://media-cdn.tripadvisor.com/media/photo-s/10/90/c9/57/cazuela-de-frijoles.jpg","3","2");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.restaurants (address,description,name,url_image,category_id,city_id)
+values ("Carrera 70 # 13A - 45 Bosques del Limonar", "Fusión, Saludable. Apto para vegetarianos, Opciones veganas, Opciones sin gluten.","El Buen Alimento","https://media-cdn.tripadvisor.com/media/photo-f/06/be/e7/6b/el-buen-alimento.jpg","5","3");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.restaurants (address,description,name,url_image,category_id,city_id)
+values ("Carrera 2 No. 11 - 34 Carrera 2 No. 11 - 34", "Contemporánea, Saludable. Apto para vegetarianos, Opciones veganas, Opciones sin gluten","Prudencia","https://media-cdn.tripadvisor.com/media/photo-s/1d/22/2b/b0/caption.jpg","5","1");
+
+insert into 0523TDPROM1C01LAED0822FT_GRUPO1.restaurants (address,description,name,url_image,category_id,city_id)
+values ("Cra 36 N. 7-60 Provenza Poblado", "Italiana, Internacional, Saludable, Pizzería, Siciliana, Del sur italiano, Mediterránea, Europea", "Massi`s Grab and Go Pizzeria","https://media-cdn.tripadvisor.com/media/photo-s/1a/07/41/f1/photo1jpg.jpg","6","2");
+
+
+
+
+
+
+
+
+
+
